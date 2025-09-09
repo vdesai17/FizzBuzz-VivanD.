1ff13d77a46687544c8ffcaf6376dc27dc7c9c27
Vivan Desai <vivandesai17@outlook.com>
2025-09-09 11:39:34 -0400

    created multiples file with a main and helper method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,21 +3,27 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
 
-        int n = 0;
-        int count= 0;
+        System.out.println(multiples(1000,3,5));
+    }
 
-        while (n != 1000){
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        n = n - 1;
+        while (n != 0){
 
-            if (n % 3 == 0){
+            if (n % a == 0){
                 count++;
-            } else if (n % 5 == 0) {
+                n--;
+            } else if (n % b == 0) {
                 count++;
+                n--;
+            } else {
+
+                n--;
             }
 
 
-            n++;
         }
-
-        System.out.println(count);
+        return count;
     }
 }
